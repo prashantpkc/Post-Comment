0ee62f79e00ccad14857b8798850f2c76179304f
prashantpkc <prashantkumarpkc@gmail.com>
2023-04-01 20:54:27 +0530

    login api done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -15,3 +15,23 @@ exports.createUser = async (req,res) => {
         res.status(500).send({status : false, message : error.message});
     }
 }
+
+exports.login = async (req, res) => {
+    try {
+        
+   
+    let {email, password }= req.body
+
+    let user = await userModel.findOne({email})
+
+    if(!user) return res.status(400).send({status:false, message: "plz provide valud email"})
+
+    let token = jwt.sign({email : user.email}, "dilip chutiya hai",{
+        expiresIn : "1h"
+    })
+
+    return res.status.send({token})
+} catch (error) {
+        res.status(500).send({status:false, message:error.message})
+}
+}
